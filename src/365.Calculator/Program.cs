2808c9d25b4a7d926b0fc33cd68c46c7fc6a04a0
Ashish Arora <ashish.612@gmail.com>
2019-08-25 03:09:12 -0500

    feature 2 - allow alternate new line delimiter

--- src/365.Calculator/Program.cs
+++ src/365.Calculator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace _365.Calculator
 {
@@ -6,24 +7,39 @@ namespace _365.Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Provide an input: ");
-
-            var input = Console.ReadLine();
+            Console.WriteLine("Provide an input separating either by a comma(,) or a new line. Press S to stop. ");
 
             try
             {
-                var adder = new Adder(new char[] { ',' });
+                var input = GetInputValues();
+                var adder = new Adder(new string[] { "," ,"\n"});
                 var sum = adder.TryAdd(input);
-                Console.WriteLine(string.Format("Total value of numbers : {0}", sum));
+                Console.WriteLine(string.Format("Sum : {0}", sum));
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
 
-
-            input = Console.ReadLine();
-
+            Console.ReadLine();
         }
+
+        private static string GetInputValues()
+        {
+            
+            var rawInput = new StringBuilder();
+            
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (string.Compare(input, "S", true) == 0)
+                    break;
+
+                rawInput.AppendLine(input);
+            }
+
+            return rawInput.ToString();
+        }
+        
     }
 }
